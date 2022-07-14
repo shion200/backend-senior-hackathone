2cbe185f325e5de4370e42bc5961b3f7f2e6dc22
shion200 <usushio2002@gmail.com>
2022-07-15 00:01:04 +0900

    Try user recognize

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
 from typing import Optional
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
 
+fake_users_db = {
+    "shion" : {
+        "username" : "shion",
+        "full_name" : "shion kyumma",
+        "email" : "usushio2002@gmail.com",
+        "password" : "ahiahi",
+        "disabled" : False,
+    },
+    "alice" : {
+        "username" : "alice",
+        "full_name" : "Alice Wonderson",
+        "email" : "alice@example.com",
+        "password": "fakehashedsecret2",
+        "disabled" : True,
+    },
+}
 app = FastAPI()
 
 @app.get("/items")
