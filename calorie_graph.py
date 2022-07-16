9165b714f1c1e0747c9c2babb64b6d6fcbe2dac4
shion200 <usushio2002@gmail.com>
2022-07-17 02:47:34 +0900

    Create calorie num

--- calorie_graph.py
+++ calorie_graph.py
@@ -1,13 +1,21 @@
-import datetime
-
 import save_data
 
-def date():
-    dt = datetime.datetime.now()
-    return dt.day
-
 calorieSum = save_data.mouth_calorie_sum()
-calorieAverage = save_data.mouth_calorie_sum()/date()
+calorieAverage = save_data.mouth_calorie_sum()/save_data.date()
+
+# print(calorieSum)
+# print(calorieAverage)
+
+def calorie_num():
+    data = {
+        "detailOfDate" : {"date" : save_data.date(),
+                          "calorie" : save_data.day_calorie_sum()},
+        "calorieAverage" : calorieAverage,
+        "calorieSum" : calorieSum
+    }
+    return data
+
+print(calorie_num())
 # print(date())
 # type detailOfDate = {
 #     "date" : number,
--- save_data.py
+++ save_data.py
@@ -1,21 +1,27 @@
 import sqlite3
 import pandas as pd
+import datetime
 
 import get_meal_calorie 
 import main
-import calorie_graph
+
+def date():
+    dt = datetime.datetime.now()
+    return dt.day
 
 name = 'shion'
-foodname = '煮込み' 
+foodname = 'カレーうどん' 
 unit1 = 757
 unit2 = 0
-calorie = 522
-date = calorie_graph.date()
+calorie = 456
+today = date()
 
 db_name = "meal_data.db"
 conn = sqlite3.connect(db_name,
                        isolation_level=None
                     )
+
+
 def data_table():
     c = conn.cursor()
     sql = """
@@ -24,12 +30,14 @@ def data_table():
             foodname VARCHAR(20),
             unit1    INTEGER,
             unit2    INTEGER,
-            calorie  INTEGER
-            date     INTEGER
+            calorie  INTEGER,
+            today     INTEGER
         );
     """ 
-    c.eecute(sql)
+    c.execute(sql)
     c.close()
+
+# data_table()
 
 # sql = f"""
 #     INSERT INTO FOOD VALUES(
@@ -38,39 +46,55 @@ def data_table():
 #         {unit1},
 #         {unit2},
 #         {calorie},
-#         {date},
+#         {today},
 #     )
 # """
 def data_write():
-    sql = (name, foodname, unit1, unit2, calorie, date)
+    sql = (name, foodname, unit1, unit2, calorie, today)
     c = conn.cursor()
     c.execute("insert into FOOD VALUES(?, ?, ?, ?, ?, ?)",sql)
-    c.commit()
-    c.in_transaction = False
+    # c.commit()
+    # c.in_transaction = False
     c.close()
 
 # c.execute(all)
-
+# data_write()
 def data_read():
+    all = """SELECT DISTINCT * FROM FOOD"""
+    c = conn.cursor()
+    c.execute(all)
+    for item_all in c:
+        print(item_all)
+        user = item_all[0]
+        if user == name:
+            print(item_all)
+    c.close()
+
+data_read()
+
+def mouth_calorie_sum():
+    ans = 0
     all = """SELECT DISTINCT * FROM FOOD"""
     c = conn.cursor()
     c.execute(all)
     for item_all in c:
         user = item_all[0]
         if user == name:
-            print(item_all)
-    c.close()
-
-def mouth_calorie_sum():
-    ans = 0
-    all = """SELECT DISTINCT * FROM FOOD"""
-    c = conn.cursor()
-    for item_all in c:
-        user = item_all[0]
-        if user == name:
             ans += item_all[4]
     return ans
 
+def day_calorie_sum():
+    ans = 0
+    all = """SELECT DISTINCT * FROM FOOD"""
+    c = conn.cursor()
+    c.execute(all)
+    for item_all in c:
+        user = item_all[0]
+        if user == name and date() == item_all[5]:
+            ans += item_all[4]
+    return ans
+    
+# print(mouth_calorie_sum())
 # def read_not_same_name():
     
 # print(sql)
