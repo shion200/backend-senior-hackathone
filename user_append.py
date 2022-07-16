95a585c644eb960fbdcbc42c8ce96b6008a77fbe
shion200 <usushio2002@gmail.com>
2022-07-16 19:03:07 +0900

    Add data Function

--- user_append.py
+++ user_append.py
@@ -24,6 +24,24 @@ example = {
     },
 }
 
+data_input= {
+    "name" : {
+        "username" : "default",
+        "hashed_password" : "default",
+    }
+}
+def change_dict_key(d, old_key, new_key, default_value=None):
+    d[new_key] = d.pop(old_key, default_value)
+
+def input_user_data(name, new_name, new_password):
+    change_dict_key(data_input, "name", name)
+    data_input.update({ name : {"username" : new_name, "hashed_password" : new_password}})
+    # data_input.update("username" : new_name,"hashed_password" : new_password})
+
+def input_data():
+    input_user_data("ahiahiahi","ahiahiahi", "enter")
+    return data_input
+    
 def no_duplication(information :dict, beginner: dict):
     flag = True
     example_keys = list(beginner.keys())
@@ -34,10 +52,10 @@ def no_duplication(information :dict, beginner: dict):
             flag == False
         
     if flag == True:
-        users_information.update(example)
+        users_information.update(beginner)
     return users_information
 
 def information_replay():
-    no_duplication(users_information, example)
+    no_duplication(users_information, input_data())
     replay = users_information
     return replay
